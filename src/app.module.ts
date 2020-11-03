bcd7a74843a281eec14de7c6ac114ce9c7b91f52
Antoine Michel <amichel@leadersleague.com>
2020-11-03 23:33:51 +0100

    feat(lib): add nestjs graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: true,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
